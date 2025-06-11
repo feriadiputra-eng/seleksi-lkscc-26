5a2e25f6742e6b48628860bb2a6506b1457b2d69
feriadiputra-eng <feriadiputraall@gmail.com>
2025-06-11 16:14:40 +0700

    Update script.js

--- apps1/frontend/script.js
+++ apps1/frontend/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         try {
             //Replace this with your API Gateway URL
-            const response = await fetch('https://agc3rnx1bg.execute-api.us-west-2.amazonaws.com/dev/app1', { 
+            const response = await fetch('https://z4xaqzsz49.execute-api.us-west-2.amazonaws.com/dev/app1', { 
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function fetchData() {
         try {
-            const response = await fetch('https://agc3rnx1bg.execute-api.us-west-2.amazonaws.com/dev/app1'); // Ganti dengan GET endpoint
+            const response = await fetch('https://z4xaqzsz49.execute-api.us-west-2.amazonaws.com/dev/app1'); // Ganti dengan GET endpoint
             const raw = await response.json();
             const dataList = JSON.parse(raw.body);
 
